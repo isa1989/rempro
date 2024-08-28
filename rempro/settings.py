7273a650de03896c9fa182fa55203ca6ffc617e0
isagtech <isar@galltech.az>
2024-08-28 15:28:04 +0400

    upd

--- rempro/settings.py
+++ rempro/settings.py
@@ -97,7 +97,7 @@ if not env.bool("PROD", default=False):
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "django_tenants.postgresql_backend",
+            "ENGINE": "django.db.backends.postgresql",
             "NAME": env("DATABASE_NAME"),
             "USER": env("DATABASE_USER"),
             "PASSWORD": env("DATABASE_PASS"),
