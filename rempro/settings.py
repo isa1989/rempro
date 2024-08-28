1a13847f33de26868e3690899b6ca6e6227365d9
isagtech <isar@galltech.az>
2024-08-28 15:56:51 +0400

    upd

--- rempro/settings.py
+++ rempro/settings.py
@@ -102,7 +102,7 @@ else:
             "USER": env("DATABASE_USER"),
             "PASSWORD": env("DATABASE_PASS"),
             "HOST": env("DATABASE_HOST"),
-            "PORT": "5432",
+            "PORT": env("DATABASE_PORT"),
         },
     }
 
