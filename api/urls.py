884c2cac6cdcfa6e48ad541be50b341aeb18c62f
isagtech <isar@galltech.az>
2024-10-18 17:58:36 +0400

    upd

--- api/urls.py
+++ api/urls.py
@@ -13,35 +13,33 @@ from api.views import (
 
 urlpatterns = [
     # -----------------------------FLAT-----------------------------------------------
-    path("api/flats", FlatListView.as_view(), name="flat-list"),
-    path("api/flats/<int:id>/charges", ChargeListView.as_view(), name="flat-charges"),
+    path("flats", FlatListView.as_view(), name="flat-list"),
+    path("flats/<int:id>/charges", ChargeListView.as_view(), name="flat-charges"),
     # -----------------------------CHARGE-----------------------------------------------
-    path("api/charges/all", AllChargesListView.as_view(), name="all-charges"),
+    path("charges/all", AllChargesListView.as_view(), name="all-charges"),
     # -----------------------------PAYMENT-----------------------------------------------
-    path("api/payments", PaymentsListView.as_view(), name="payments-list"),
+    path("payments", PaymentsListView.as_view(), name="payments-list"),
     # -----------------------------GARAGE-----------------------------------------------
-    path("api/garages", GaragesListView.as_view(), name="garages-list"),
+    path("garages", GaragesListView.as_view(), name="garages-list"),
     # -----------------------------NOTİFİCATİON-----------------------------------------------
+    path("notifications", NotificationsListView.as_view(), name="notifications-list"),
     path(
-        "api/notifications", NotificationsListView.as_view(), name="notifications-list"
-    ),
-    path(
-        "api/notifications/<int:id>",
+        "notifications/<int:id>",
         NotificationDetailView.as_view(),
         name="notification-detail",
     ),
     path(
-        "api/notifications/<int:id>",
+        "notifications/<int:id>",
         NotificationDetailView.as_view(),
         name="notification-detail",
     ),
     path(
-        "api/notifications/count",
+        "notifications/count",
         UnreadNotificationsCountView.as_view(),
         name="unread-notifications-count",
     ),
     path(
-        "api/notifications/read",
+        "notifications/read",
         MarkAllNotificationsReadView.as_view(),
         name="mark-all-notifications-read",
     ),
